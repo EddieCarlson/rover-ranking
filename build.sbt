413eeb7490dece123a8b7ff584561299a8067fa3
Candidate <candidate@rover.com>
2021-05-15 12:25:13 -0700

    project init

---
+++ build.sbt
@@ -0,0 +1,5 @@
+name := "rover-ranking"
+
+version := "0.1"
+
+scalaVersion := "2.13.5"
---
+++ src/main/scala/Main.scala
@@ -0,0 +1,8 @@
+object Main extends App {
+
+  // TODO: make safe
+  val filepath = args.head
+
+  println(filepath)
+
+}
