e7fa5b15477b074e984c8feaece40cb195fe7e6b
Candidate <candidate@rover.com>
2021-05-17 14:42:56 -0700

    separate file for error messaging helpers

--- src/main/scala/rover/SitterReview.scala
+++ src/main/scala/rover/SitterReview.scala
@@ -3,9 +3,10 @@ package rover
 import scala.io.Source
 import scala.util.{Try, Using}
 
-import cats.data.{NonEmptyList, Validated, ValidatedNel}
+import cats.data.{Validated, ValidatedNel}
 import cats.implicits._
 
+import SitterReviewErrorMessaging._
 import SitterReviewValidators.validateReview
 
 // contains the relevant information to be gleaned from each row in the input dataset
@@ -50,31 +51,6 @@ object SitterReviewParsers {
     case _ =>
       s"line did not contain 14 elements: ${line.mkString(",")}".invalidNel
   }
-
-  // ---- error message embellishment helpers ----
-
-  // gathers all error messages from parsing/validating and generates a Throwable with a message containing all error
-  // messages separated by newlines (truncated at 50 so as to not be obnoxious)
-  def combineErrors(errorMessages: NonEmptyList[String]): Throwable = {
-    val numErrors = errorMessages.length
-    val truncatedMsg =
-      if (numErrors > 50) s"\n...truncated at 50. contained $numErrors errors"
-      else ""
-    val allErrorsMsg = errorMessages.toList.take(50).mkString("\n")
-    new IllegalArgumentException(s"errors during parsing:\n$allErrorsMsg$truncatedMsg")
-  }
-
-  def addErrorRowIndices[A](list: List[ValidatedNel[String, A]]): List[ValidatedNel[String, A]] =
-    list.zipWithIndex.map { case (v, idx) => // for any validations that are failures, add row number to err msg
-      v.leftMap(msgs => msgs.map(msg => s"(row ${idx + 1}) $msg"))
-    }
-
-  // add offending input row to a validation error message. if a validation for a single row in the csv has multiple
-  // validation errors, include the row only in the first error msg, rather than duplicating it in all msgs for the row
-  def addErrorLineOnce[A](v: ValidatedNel[String, A], line: String): ValidatedNel[String, A] =
-    v.leftMap { case NonEmptyList(head, tail) =>
-      NonEmptyList(s"$head in line: $line", tail)
-    }
 }
 
 // simple validators that take parsed strings and turn them into SitterReview (components)
---
+++ src/main/scala/rover/SitterReviewErrorMessaging.scala
@@ -0,0 +1,31 @@
+package rover
+
+import cats.data.{NonEmptyList, ValidatedNel}
+
+// helpers to add useful, contextual information to error messages generated from parsing/validating SitterReviews
+object SitterReviewErrorMessaging {
+  // gathers all error messages from parsing/validating and generates a Throwable with a message containing all error
+  // messages separated by newlines (truncated at 50 so as to not be obnoxious)
+  def combineErrors(errorMessages: NonEmptyList[String]): Throwable = {
+    val numErrors = errorMessages.length
+    val truncatedMsg =
+      if (numErrors > 50) s"\n...truncated at 50. contained $numErrors errors"
+      else ""
+    val allErrorsMsg = errorMessages.toList.take(50).mkString("\n")
+    new IllegalArgumentException(s"errors during parsing:\n$allErrorsMsg$truncatedMsg")
+  }
+
+  // for any validations that are Invalid, add row number to error msg
+  def addErrorRowIndices[A](list: List[ValidatedNel[String, A]]): List[ValidatedNel[String, A]] =
+    list.zipWithIndex.map { case (v, idx) =>
+      v.leftMap(msgs => msgs.map(msg => s"(row ${idx + 1}) $msg"))
+    }
+
+  // add offending input row to the validation error message generated when attempting to validate that row. if a
+  // validation for a single row in the csv has multiple validation errors, include the input row only in the first
+  // error msg, rather than duplicating it in all msgs for the row
+  def addErrorLineOnce[A](v: ValidatedNel[String, A], line: String): ValidatedNel[String, A] =
+    v.leftMap { case NonEmptyList(head, tail) =>
+      NonEmptyList(s"$head in line: $line", tail)
+    }
+}
